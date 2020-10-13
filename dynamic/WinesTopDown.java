3aaeb30147232854200d676cdf9c7e8f677c4e6c
Abhinav Rana <ranaabhinav50@gmail.com>
2020-10-13 13:35:15 +0530

    Dynamic Programming
    Revising Dynamic Programming .
    I'm revising dynamic programming so more questions are to come here :)
    
    Update : Selling Vines Problem.

---
+++ dynamic/WinesTopDown.java
@@ -0,0 +1,36 @@
+package dynamic;
+
+public class WinesTopDown {
+	static int countprofit(int i , int j , int year, int wines[],int dp[][])
+	{
+		//year denotes which year it is. 
+		//wines stores bottle values. 
+		// double dimension array will be used. 
+		//Base Case
+		// i > j will be our base case it means that no bottle is left
+		if(i>j)
+		{
+			return 0;
+		}
+		
+		
+		if(dp[i][j] != 0)
+		{
+			return dp[i][j];
+		}
+		//Recursive Case
+		int op1 = wines[i]*year + countprofit(i+1, j , year++, wines,dp);
+		int op2 = wines[i]*year + countprofit(i, j-1, year++, wines,dp);
+		
+		return dp[i][j] = Math.max(op1, op2);
+		
+	}
+	
+	public static void main(String args[]) {
+		int year = 1;
+		int wines[] = {2,3,5,1,4};
+		int dp[][] = new int [100][100];
+		
+		System.out.println("Profit => "+countprofit(0, wines.length-1 , 1, wines,dp));
+	}
+}

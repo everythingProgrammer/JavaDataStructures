9e7bd8d4b84103b2ae27c44f5f8f4e02bf677f71
Abhinav Rana <ranaabhinav50@gmail.com>
2020-10-18 15:15:54 +0530

    Update RodCutting.java
    Bottoms up approach of RodCutting.

--- dynamic/RodCutting.java
+++ dynamic/RodCutting.java
@@ -7,7 +7,7 @@ public class RodCutting {
 	
 	public static int price(int len , int pricearr[], int dp[] ) {
 		
-		if(len == 0) {
+		if(len <= 0) {
 			return 0;
 		}
 		if(dp[len-1] != 0) {
@@ -17,16 +17,36 @@ public class RodCutting {
 		for(int j= 1 ; j<= len ; j++) {
 			dp[len-1] = Math.max(  Math.max(pricearr[len-1] , price(len - j, pricearr, dp) + dp[j-1]), dp[len-1]);
 			 
-		}
+		}		
 		return dp[len-1];
  
 	}
+	
+	
+	
+	public static int bottomsUp(int len, int price[]) {
+		int dp[] = new int [len+1];
+		
+		
+		for(int i = 1 ; i<= len ; i++) {
+			int max_val = Integer.MIN_VALUE;
+			for(int j=0; j<i ; j++) {
+				max_val = Math.max(max_val , price[j] + dp[i-j-1]);
+			}
+			dp[i] = max_val;
+		}
+		return dp[len ];
+		
+		
+	}
+	
+	
 	public static void main(String args[]) {
 		int pricearr[] = {1,5,8,9,10,17,17,15};
 		int dp[] = new int[pricearr.length];
 		 
 		dp[0] = pricearr[0];
-		System.out.println( price(8 , pricearr,dp) );
+		System.out.println( bottomsUp(8, pricearr));
 	}
 	
 }

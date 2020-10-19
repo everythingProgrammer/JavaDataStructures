ab8ad704252cab6a787f03b2ec84288d0b9b812b
Abhinav Rana <ranaabhinav50@gmail.com>
2020-10-20 00:33:43 +0530

    Update ProfitMaximization.java
    Correction  made, return maximum Profit.

--- dynamic/ProfitMaximization.java
+++ dynamic/ProfitMaximization.java
@@ -5,12 +5,18 @@ public class ProfitMaximization {
 	
 	public static int maxProfit(int nums[]) {
 		int dp[] = new int[nums.length];
+		
+		int max_profit = 0;
 		dp[0] = nums[0];
 		
 		for(int i = 1 ; i<nums.length ; i++) {
 			for(int j = 0; j<i ; j++) {
 				if(nums[i]%nums[j] == 0) {
+					
 					dp[i] = Math.max(nums[i], dp[j] + nums[i]);
+					if(dp[i] > max_profit) {
+						max_profit= dp[i];
+					}
 				}
 			}
 		}
@@ -19,7 +25,7 @@ public class ProfitMaximization {
 //		}
 //		
 		
-		return dp[dp.length-1];
+		return max_profit;
 	}
 	
 	public static void main(String args[]) {

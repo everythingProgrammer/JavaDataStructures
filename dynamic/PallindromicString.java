bd77f6eb961c0591a22805d52deef85b183ccb38
Abhinav Rana <ranaabhinav50@gmail.com>
2020-10-24 23:41:15 +0530

    update Pallindromic Substring

--- dynamic/PallindromicString.java
+++ dynamic/PallindromicString.java
@@ -3,18 +3,25 @@ package dynamic;
 public class PallindromicString {
 	
 	
-	public static int pallindromicString(String str, int l , int r) {
+	public static boolean pallindromicString(String str, int l , int r) {
+		/*Recurrence relation function(r,l) = function(l+1, r-1) && s[l] == s[r]*/
+		if(l>r) {
+			return false;
+		}
+		
+		if(l == r || (l== (r-1) && str.charAt(l) == str.charAt(r))) {
+			return true;
+		}
 		
 		
 		
-		
-		
-		return 0;
+		return pallindromicString(str, l+1, r-1) && (str.charAt(l)== str.charAt(r));
 	}
 	
 	
 	public static void main(String args[]) {
-		
+		/*indexing has to be 0 based in this. so adjust accordingly.*/
+		System.out.println(pallindromicString("abbac", 1,4));
 		
 		
 	}

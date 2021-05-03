1d1234be7f7ccf3b89088729a3110ae0f447a39e
Abhinav Rana <ranaabhinav50@gmail.com>
2021-05-04 00:08:01 +0530

    Create MaximumXORPair.java

---
+++ arrays/MaximumXORPair.java
@@ -0,0 +1,28 @@
+package arrays;
+import java.util.*;
+public class MaximumXORPair {
+	
+	/*using trie structure...*/
+	
+	public int findMaximumXOR(int[] nums) {
+		
+		return 0;
+		
+	}
+	
+	public static void main(String args[]) {
+		Scanner sr = new Scanner(System.in);
+		int n = sr.nextInt();
+//		int num[] = new int[n];
+		int nums[] = {3,10,5,25,2,8};
+		
+		
+		/*To form largest XOR pair we need opposite bits
+		 * 
+		 * */
+		
+		
+	}
+	
+	
+}
